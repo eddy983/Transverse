5d2d103270c498ee1b4f57d75a5418969822447a
eddy983 <edidiongisong7@gmail.com>
2018-07-04 13:31:58 +0100

    made index page responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,28 @@
 /* Scroll Feature */
+flip = document.getElementsByClassName("flip")[0];
+flip2 = document.getElementsByClassName("flip2")[0];
+flip3 = document.getElementsByClassName("flip3")[0];
+flip4 = document.getElementsByClassName("flip4")[0];
+console.log(flip2.offsetTop)
 
+console.dir(flip)
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {
   scrollFunction();
+  // console.dir(flip)
+  if (window.pageYOffset >= flip.offsetTop-200) {
+    console.log(window.pageYOffset)
+    flip.style.animation = "rotate 3s ease 1s backwards";
+    flip2.style.animation = "rotate 3s ease 2s backwards";
+    flip3.style.animation = "rotate 3s ease 3s backwards";
+    flip4.style.animation = "rotate 3s ease 4s backwards";
+    flip.style.display = "block";
+    flip2.style.display = "block";
+    flip3.style.display = "block";
+    flip4.style.display = "block";
+    
+    console.log(flip2.offsetTop)
+  }
 };
 
 function scrollFunction() {
